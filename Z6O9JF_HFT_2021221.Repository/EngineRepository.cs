1915b514ae7066cf4102f7e34fda83065cf71987
AlbertCsipak <csipakalbert@stud.uni-obuda.hu>
2021-10-14 22:21:25 +0200

    EngineRepository added

--- Z6O9JF_HFT_2021221.Repository/EngineRepository.cs
+++ Z6O9JF_HFT_2021221.Repository/EngineRepository.cs
@@ -1,12 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
+using Z6O9JF_HFT_2021221.Data;
+using Z6O9JF_HFT_2021221.Models;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Z6O9JF_HFT_2021221.Repository
 {
     class EngineRepository
     {
+        MyDbContext dataBase;
+
+        public EngineRepository(MyDbContext database)
+        {
+            this.dataBase = database;
+        }
+
+        public void Create(Engine entity)
+        {
+            dataBase.Engine.Add(entity);
+            dataBase.SaveChanges();
+        }
+
+        public Engine Read(int id)
+        {
+            return dataBase.Engine.FirstOrDefault(entity => entity.EngineCode == id);
+        }
+
+        public IQueryable<Engine> GetAll()
+        {
+            return dataBase.Engine;
+        }
+
+        public void Delete(int id)
+        {
+            var entity = Read(id);
+            dataBase.Engine.Remove(entity);
+            dataBase.SaveChanges();
+        }
+
+        public void Update(Engine entity)
+        {
+            var entityToUpdate = Read(entity.EngineCode);
+            entityToUpdate.BrandId = entity.BrandId;
+            entityToUpdate.Displacement = entity.Displacement;
+            entityToUpdate.EngineType = entity.EngineType;
+            entityToUpdate.Power = entity.Power;
+
+            dataBase.SaveChanges();
+        }
     }
 }
