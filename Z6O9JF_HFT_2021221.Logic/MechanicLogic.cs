20546a0810dbdd14272b36c7506cc11cb9c9501f
AlbertCsipak <csipakalbert@stud.uni-obuda.hu>
2021-10-14 23:54:55 +0200

    MechanicLogic added

--- Z6O9JF_HFT_2021221.Logic/MechanicLogic.cs
+++ Z6O9JF_HFT_2021221.Logic/MechanicLogic.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Z6O9JF_HFT_2021221.Repository;
+using Z6O9JF_HFT_2021221.Models;
 
 namespace Z6O9JF_HFT_2021221.Logic
 {
-    class MechanicLogic
+    public class MechanicLogic
     {
+        IMechanicRepository myRepository;
+        public MechanicLogic(IMechanicRepository entity)
+        {
+            this.myRepository = entity;
+        }
+        public IEnumerable<Mechanic> GetAll()
+        {
+            return myRepository.GetAll();
+        }
+        public void Create(Mechanic entity)
+        {
+            myRepository.Create(entity);
+        }
+        public Mechanic Read(int id)
+        {
+            return myRepository.Read(id);
+        }
+        public void Update(Mechanic entity)
+        {
+            myRepository.Update(entity);
+        }
+        public void Delete(int id)
+        {
+            myRepository.Delete(id);
+        }
     }
 }
