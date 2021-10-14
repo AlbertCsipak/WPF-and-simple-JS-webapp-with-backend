3d112f02148aac239b6850746b6aaf71fd916c42
AlbertCsipak <csipakalbert@stud.uni-obuda.hu>
2021-10-14 13:11:37 +0200

    Primary keys added

--- Z6O9JF_HFT_2021221.Models/Owner.cs
+++ Z6O9JF_HFT_2021221.Models/Owner.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,8 @@ namespace Z6O9JF_HFT_2021221.Models
 {
     public class Owner
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int OwnerId { get; set; }
         public string Name { get; set; }
     }
