7fdeab4f7b68ffc40928cbff2bf4702f4098842a
AlbertCsipak <csipakalbert@stud.uni-obuda.hu>
2021-10-14 23:51:32 +0200

    CarServiceLogic added

--- Z6O9JF_HFT_2021221.Logic/CarServiceLogic.cs
+++ Z6O9JF_HFT_2021221.Logic/CarServiceLogic.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Z6O9JF_HFT_2021221.Repository;
+using Z6O9JF_HFT_2021221.Models;
 
 namespace Z6O9JF_HFT_2021221.Logic
 {
-    class CarServiceLogic
+    public class CarServiceLogic
     {
+        ICarServiceRepository myRepository;
+        public CarServiceLogic(ICarServiceRepository entity)
+        {
+            this.myRepository = entity;
+        }
+        public IEnumerable<CarService> GetAll()
+        {
+            return myRepository.GetAll();
+        }
+        public void Create(CarService entity)
+        {
+            myRepository.Create(entity);
+        }
+        public CarService Read(int id)
+        {
+            return myRepository.Read(id);
+        }
+        public void Update(CarService entity)
+        {
+            myRepository.Update(entity);
+        }
+        public void Delete(int id)
+        {
+            myRepository.Delete(id);
+        }
     }
 }
