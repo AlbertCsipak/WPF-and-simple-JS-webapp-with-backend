ac2d9327b9b2b33fbe614127c76c1c6fafbdc22f
AlbertCsipak <csipakalbert@stud.uni-obuda.hu>
2021-10-14 23:52:36 +0200

    EngineLogic added

--- Z6O9JF_HFT_2021221.Logic/EngineLogic.cs
+++ Z6O9JF_HFT_2021221.Logic/EngineLogic.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Z6O9JF_HFT_2021221.Repository;
+using Z6O9JF_HFT_2021221.Models;
 
 namespace Z6O9JF_HFT_2021221.Logic
 {
-    class EngineLogic
+    public class EngineLogic
     {
+        IEngineRepository myRepository;
+        public EngineLogic(IEngineRepository entity)
+        {
+            this.myRepository = entity;
+        }
+        public IEnumerable<Engine> GetAll()
+        {
+            return myRepository.GetAll();
+        }
+        public void Create(Engine entity)
+        {
+            myRepository.Create(entity);
+        }
+        public Engine Read(int id)
+        {
+            return myRepository.Read(id);
+        }
+        public void Update(Engine entity)
+        {
+            myRepository.Update(entity);
+        }
+        public void Delete(int id)
+        {
+            myRepository.Delete(id);
+        }
     }
 }
