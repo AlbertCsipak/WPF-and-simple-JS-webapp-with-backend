ed9edb66f8e957a6f3bff7c466ce0c3e549000c6
AlbertCsipak <csipakalbert@stud.uni-obuda.hu>
2021-10-14 13:05:42 +0200

    Engine Class Modified

--- Z6O9JF_HFT_2021221.Models/Car.cs
+++ Z6O9JF_HFT_2021221.Models/Car.cs
@@ -13,9 +13,7 @@ namespace Z6O9JF_HFT_2021221.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Vin { get; set; }
-        public enum BodyStyleEnum { Sedan, Hatchback, Coupe, Wagon }
-        public BodyStyleEnum BodyStyle { get; set; }
-        public enum ColorEnum {Green,Blue,Black,White,Gray,Yellow,Red }
-        public ColorEnum Color { get; set; }
+        public Enums.BodyStyleEnum BodyStyle { get; set; }
+        public Enums.ColorEnum Color { get; set; }
     }
 }
---
+++ Z6O9JF_HFT_2021221.Models/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z6O9JF_HFT_2021221.Models
+{
+    public class Enums
+    {
+        public enum ColorEnum { Green, Blue, Black, White, Gray, Yellow, Red }
+        public enum BodyStyleEnum { Sedan, Hatchback, Coupe, Wagon }
+        public enum EngineType { Gasoline, Petrol, Electric, Hybrid }
+    }
+}
