931cdae469eda3a346c6639c79aa8a5cb4b224c9
AlbertCsipak <csipakalbert@stud.uni-obuda.hu>
2021-10-15 10:46:02 +0200

    Added nuget packages to Test

--- Z6O9JF_HFT_2021221.Client/Program.cs
+++ Z6O9JF_HFT_2021221.Client/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Z6O9JF_HFT_2021221.Data;
+using Z6O9JF_HFT_2021221.Logic;
+using Z6O9JF_HFT_2021221.Repository;
+using Z6O9JF_HFT_2021221.Models;
 
 namespace Z6O9JF_HFT_2021221.Client
 {
@@ -8,6 +11,11 @@ namespace Z6O9JF_HFT_2021221.Client
         static void Main(string[] args)
         {
             MyDbContext db = new();
+
+            AdvancedLogic advanced = new(new BrandRepository(db),new CarRepository(db),new MechanicRepository(db),new OwnerRepository(db),
+                new CarServiceRepository(db),new EngineRepository(db));
+
+            var a1 = advanced.AVGPriceByBrands();
             db.SaveChanges();
             ;
         }
--- Z6O9JF_HFT_2021221.Logic/AdvancedLogic.cs
+++ Z6O9JF_HFT_2021221.Logic/AdvancedLogic.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using Z6O9JF_HFT_2021221.Repository;
 using Z6O9JF_HFT_2021221.Models;
+using System.Linq;
 
 namespace Z6O9JF_HFT_2021221.Logic
 {
-    class AdvancedLogic
+    public class AdvancedLogic
     {
+        IBrandRepository brandRepo;
+        ICarRepository carRepo;
+        IMechanicRepository mechanicRepo;
+        IOwnerRepository ownerRepo;
+        ICarServiceRepository carServiceRepo;
+        IEngineRepository engineRepo;
+
+        public AdvancedLogic(IBrandRepository brandRepository, ICarRepository carRepository, IMechanicRepository mechanicRepository,
+            IOwnerRepository ownerRepository, ICarServiceRepository carServiceRepository, IEngineRepository engineRepository)
+        {
+            this.brandRepo = brandRepository;
+            this.carRepo = carRepository;
+            this.mechanicRepo = mechanicRepository;
+            this.ownerRepo = ownerRepository;
+            this.carServiceRepo = carServiceRepository;
+            this.engineRepo = engineRepository;
+        }
+
+        public IEnumerable<KeyValuePair<string, double>> AVGPriceByBrands()
+        {
+            return from car in carRepo.GetAll() group car by car.Brand.Name into carg 
+                   select new KeyValuePair<string, double> (carg.Key, carg.Average(car => car.ServiceCost));
+        }
     }
 }
