e6a55eb9a7dec38597a765cc2363cd76aeda46dd
AlbertCsipak <csipakalbert@stud.uni-obuda.hu>
2021-10-18 00:36:19 +0200

    Advanced non-crud method added

--- Z6O9JF_HFT_2021221.Client/Program.cs
+++ Z6O9JF_HFT_2021221.Client/Program.cs
@@ -12,11 +12,33 @@ namespace Z6O9JF_HFT_2021221.Client
         {
             MyDbContext db = new();
 
-            AdvancedLogic advanced = new(new BrandRepository(db),new CarRepository(db),new MechanicRepository(db),new OwnerRepository(db),
-                new CarServiceRepository(db),new EngineRepository(db));
+            //ICarRepository carRepo = new CarRepository(db);
 
-            var a1 = advanced.AVGPriceByBrands();
-            db.SaveChanges();
+            //carRepo.Create(new Car()
+            //{
+            //    BodyStyle = Enums.BodyStyleEnum.Sedan,
+            //    BrandId = 1,
+            //    Color = Enums.ColorEnum.Red,
+            //    EngineCode = 5123123,
+            //    MechanicId = 1,
+            //    OwnerId = 1,
+            //    Model = "anyad",
+            //    ServiceCost = 1500
+            //});
+            //var a2 = carRepo.GetAll();
+
+            AdvancedLogic advanced = new(new BrandRepository(db), new CarRepository(db), new MechanicRepository(db), new OwnerRepository(db),
+                new CarServiceRepository(db), new EngineRepository(db));
+
+            //var a1 = advanced.AVGServiceCostByBrands();
+
+            //var a2 = advanced.HorsePower();
+
+            //var a3 = advanced.MostCommonEngineType();
+
+            //var a4 = advanced.ServiceIncome();
+
+            var a5 = advanced.CarBrandsInService();
             ;
         }
     }
--- Z6O9JF_HFT_2021221.Logic/AdvancedLogic.cs
+++ Z6O9JF_HFT_2021221.Logic/AdvancedLogic.cs
@@ -26,10 +26,50 @@ namespace Z6O9JF_HFT_2021221.Logic
             this.engineRepo = engineRepository;
         }
 
-        public IEnumerable<KeyValuePair<string, double>> AVGPriceByBrands()
+        public IEnumerable<KeyValuePair<string, double>> AVGServiceCostByBrands()
         {
-            return from car in carRepo.GetAll() group car by car.Brand.Name into carg 
-                   select new KeyValuePair<string, double> (carg.Key, carg.Average(car => car.ServiceCost));
+            return from car in carRepo.GetAll()
+                   group car by car.Brand.Name into carg
+                   select new KeyValuePair<string, double>(carg.Key, carg.Average(car => car.ServiceCost));
+        }
+        public IEnumerable<KeyValuePair<Enums.EngineType, int>> MostCommonEngineType()
+        {
+            return from engine in engineRepo.GetAll()
+                   group engine by engine.EngineType into engineg
+                   select new KeyValuePair<Enums.EngineType, int>(engineg.Key, engineg.Count());
+        }
+        public IEnumerable<KeyValuePair<int, double>> HorsePower()
+        {
+            return from car in carRepo.GetAll()
+                   select new KeyValuePair<int, double>(car.Vin, car.Engine.Power);
+        }
+        public IEnumerable<KeyValuePair<string,double>> ServiceIncome()
+        {
+            return from car in carRepo.GetAll()
+                   group car by car.Mechanic.CarService.Name into carg
+                   select new KeyValuePair<string, double>(carg.Key, carg.Sum(entity => entity.ServiceCost));
+        }
+        public IEnumerable<KeyValuePair<string, List<Brand>>> CarBrandsInService()
+        {
+            var a1 = carRepo.GetAll().AsEnumerable().GroupBy(car => car.Mechanic.CarService.Name);
+
+            List<Brand> brands = new();
+            List<KeyValuePair<string, List<Brand>>> services = new();
+
+            foreach (var item in a1)
+            {
+                foreach (var item1 in item)
+                {
+                    if (!brands.Contains(item1.Brand))
+                    {
+                        brands.Add(item1.Brand);
+                    }
+                }
+                services.Add(new KeyValuePair<string, List<Brand>>(item.Key, brands));
+                brands = null;
+            }
+            ;
+            return services;
         }
     }
 }
